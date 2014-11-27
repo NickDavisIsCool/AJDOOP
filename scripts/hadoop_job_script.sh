c75ffb0314085695bc857a84ec2af9dc928884a5
Nick Davis <ndavis92x@gmail.com>
2014-11-27 00:31:45 -0700

    turned out hadoop script was overwritten with ajira script for some reason, had to re-write that real quick

--- scripts/hadoop_job_script.sh
+++ scripts/hadoop_job_script.sh
@@ -1,14 +1,27 @@
-program="nl.vu.cs.ajira.examples.WordCount"
 FILES=test1
-FILE_INPUT_DIR=/home/hduser/test_files/$FILES
-FILE_OUTPUT_DIR=/home/hduser/output/ajira/$FILES
-LAUNCH=/home/hduser/ajira/scripts/launch.sh
+LOCAL_FILE_INPUT_DIR=/home/hduser/test_files/$FILES
+LOCAL_FILE_OUTPUT_DIR=/home/hduser/output/hadoop
+DFS_FILE_INPUT_DIR=/user/hduser/test_files
+DFS_FILE_OUTPUT_DIR=/user/hduser/output/hadoop/$FILES
+JAR=/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar
+program="wordcount"
 
-rm -rf $FILE_OUTPUT_DIR
+#start-all.sh
+
+rm -rf $LOCAL_FILE_OUTPUT_DIR/$FILES
+hdfs dfs -rm -r $DFS_FILE_OUTPUT_DIR
+
+hdfs dfs -copyFromLocal $LOCAL_FILE_INPUT_DIR $DFS_FILE_INPUT_DIR
 
 START=$(date +%s.%N)
-$LAUNCH -Dibis.pool.size=1 -Dibis.serverAddress=localhost $program $FILE_INPUT_DIR  $FILE_OUTPUT_DIR
+hadoop jar $JAR $program $DFS_FILE_INPUT_DIR/$FILES $DFS_FILE_OUTPUT_DIR
 
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
-echo $DIFF
+
+hdfs dfs -get $DFS_FILE_OUTPUT_DIR $LOCAL_FILE_OUTPUT_DIR
+
+#stop-all.sh
+
+
+#echo $DIFF
