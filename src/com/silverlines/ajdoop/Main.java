823050b133f815d08ae0d3a7a2391524caf2ab8d
Nick Davis <ndavis92x@gmail.com>
2014-11-28 00:41:16 -0700

    got multiple linear regression in, read to data files, exports data to data files.  Should execute optimally based off of regression info.  Don't have it working 100% modularly, but the multiple regression and file input stuff are somewhat modular?  Would be nicer if the data input/output was more modular.  Could write some methods to get rid of reproduced output/input code, but whatevs.  Will test tomorrow, actually haven't tested this yet

--- src/com/silverlines/ajdoop/Main.java
+++ src/com/silverlines/ajdoop/Main.java
@@ -10,9 +10,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 	
 	    //Read in arguments and get things set up
 	    //Make sure the user supplies all 4 arguments
@@ -32,6 +36,9 @@ public class Main {
 	    
 	    //The script which will execute the ajira implementation of the problem
 	    String ajiraScriptFileName = args[4];
+	    
+	    String reg_fields = "intercept,data_size";
+	    String data_fields = "time,data_size";
 
 	    
 	    // --------------------------------------------------------------------- //
@@ -61,9 +68,9 @@ public class Main {
 	    //	Does:
 	    //		1. Creates a directory for the program name given on input
 	    //		2. Creates a dummy regression file with two lines of 1 0.
-	    //			- first line is hadoop equation, second is ajira
+	    //			- first line is variables, then hadoop equation, then ajira
 	    // 		3. Creates 2 data files for hadoop and ajira
-	    //			- first line is # of data points in file, rest is csv data
+	    //			- just csv data
 	    //	Assumes:
 	    //		1. programType is a string with only [a-z][A-Z][0-9] chars
 	    // --------------------------------------------------------------------- //
@@ -79,19 +86,19 @@ public class Main {
 	    if(!regInfo.isFile()){
 	    	regInfo.createNewFile();
 	    	BufferedWriter bw = new BufferedWriter(new FileWriter(regInfo.getAbsoluteFile()));
-	    	bw.write("0\n0");
+	    	bw.write(reg_fields + "\n0\n0");
 	    	bw.close();
 	    }
 	    if(!hadInfo.isFile()){
 	    	hadInfo.createNewFile();
 	    	BufferedWriter bw = new BufferedWriter(new FileWriter(hadInfo.getAbsoluteFile()));
-	    	bw.write("0");
+	    	bw.write(data_fields);
 	    	bw.close();
 	    }
 	    if(!ajiInfo.isFile()){
 	    	ajiInfo.createNewFile();
 	    	BufferedWriter bw = new BufferedWriter(new FileWriter(ajiInfo.getAbsoluteFile()));
-	    	bw.write("0");
+	    	bw.write(data_fields);
 	    	bw.close();
 	    }
 	  
@@ -113,17 +120,211 @@ public class Main {
 	    // --------------------------------------------------------------------- //	    
 	    
 	    BufferedReader br = new BufferedReader(new FileReader(regInfo));
+	    String variables = br.readLine();
 	    String hadoop_reg = br.readLine();
 	    String ajira_reg = br.readLine();
+	    int numFields = reg_fields.split(",").length;
+	    double[][] x = null;
+	    double[] y = null;
+	    
+	    OLSMultipleLinearRegression mlr = new OLSMultipleLinearRegression();
+	    
+	    double[] input_data = {1, (double)size};
+	    int i, j;
 	    
 	    if(hadoop_reg.equals("0")){
 	    	//execute hadoop script, get time data, calculate regression formula, write to file
+	    	ProcessBuilder pb = new ProcessBuilder(hadoopScriptFileName);
+	    	Process p = pb.start();
+	    	p.waitFor();
+	    	String time = System.getenv("EXEC_TIME");
+	    	
+	    	String[] reg_data = {time, "0"};
+	    	String[] output_data = {time, Long.toString(size)};
+	    	
+	    	BufferedWriter bw = new BufferedWriter(new FileWriter(regInfo.getAbsoluteFile()));
+	    	bw.write(reg_fields + "\n");
+	    	for(i = 0; i < numFields; i++){
+	    		if(i == numFields -1){
+	    			bw.write(reg_data[i] + "\n");
+	    		}
+	    		else{
+	    			bw.write(reg_data[i] + ",");
+	    		}
+	    	}
+	    	bw.write(ajira_reg);
+	    	
+	    	bw = new BufferedWriter(new FileWriter(hadInfo.getAbsoluteFile(), true));
+	    	for(i = 0; i < numFields; i++){
+	    		if(i == numFields -1){
+	    			bw.write(output_data[i] + "\n");
+	    		}
+	    		else{
+	    			bw.write(output_data[i] + ",");
+	    		}
+	    	}
 	    }
 	    else if(ajira_reg.equals("0")){
 	    	//execute ajira script, get time data, calculate regression formula, write to file
+	    	ProcessBuilder pb = new ProcessBuilder(ajiraScriptFileName);
+	    	Process p = pb.start();
+	    	p.waitFor();
+	    	String time = System.getenv("EXEC_TIME");
+	    	
+	    	String[] reg_data = {time, "0"};
+	    	String[] output_data = {time, Long.toString(size)};
+	    	
+	    	BufferedWriter bw = new BufferedWriter(new FileWriter(regInfo.getAbsoluteFile()));
+	    	bw.write(reg_fields + "\n");
+	    	bw.write(hadoop_reg + "\n");
+	    	for(i = 0; i < numFields; i++){
+	    		if(i == numFields -1){
+	    			bw.write(reg_data[i] + "\n");
+	    		}
+	    		else{
+	    			bw.write(reg_data[i] + ",");
+	    		}
+	    	}
+	    	
+	    	bw = new BufferedWriter(new FileWriter(ajiInfo.getAbsoluteFile(), true));
+	    	for(i = 0; i < numFields; i++){
+	    		if(i == numFields -1){
+	    			bw.write(output_data[i] + "\n");
+	    		}
+	    		else{
+	    			bw.write(output_data[i] + ",");
+	    		}
+	    	}
+	    	
 	    }
 	    else{
 	    	//take regression info, calculate most optimal, run that script, get time data, re-calculate, write to file
+	    	String[] hadoop_vars = hadoop_reg.split(",");
+	    	String[] ajira_vars = ajira_reg.split(",");
+	    	double hadoop_sum = 0;
+	    	double ajira_sum = 0;
+	    	for(i = 0; i < hadoop_vars.length; i++){
+	    		hadoop_sum += Double.parseDouble(hadoop_vars[i])*input_data[i];
+	    		ajira_sum += Double.parseDouble(ajira_vars[i])*input_data[i];
+	    	}
+	    	
+	    	if(hadoop_sum < ajira_sum){
+		    	ProcessBuilder pb = new ProcessBuilder(hadoopScriptFileName);
+		    	Process p = pb.start();
+		    	p.waitFor();
+		    	String time = System.getenv("EXEC_TIME");
+		    	
+		    	String[] output_data = {time, Long.toString(size)};
+		    	
+			    br = new BufferedReader(new FileReader(hadInfo));
+			    br.readLine();
+			    String line;
+			    j = 0;
+			    while((line = br.readLine()) != null){
+			    	String[] tuple = line.split(",");
+			    	for(i = 0; i < numFields; i++){
+			    		if(i == 0){
+			    			y[j] = Double.parseDouble(tuple[i]);
+			    		}
+			    		else{
+			    			x[j][i-1] = Double.parseDouble(tuple[i]);
+			    		}
+			    	}
+			    	j++;
+			    }   
+			    for(i = 0; i < numFields; i ++){
+			    	if(i == 0){
+			    		y[j] = Double.parseDouble(output_data[i]);
+			    	}
+			    	x[j][i-1] = Double.parseDouble(output_data[i]);
+			    }
+			    
+			    mlr.newSampleData(y,x);
+			    double[] params = mlr.estimateRegressionParameters();
+			    
+		    	String[] reg_data = {Double.toString(params[0]), Double.toString(params[1])};
+		    	
+		    	BufferedWriter bw = new BufferedWriter(new FileWriter(regInfo.getAbsoluteFile()));
+		    	bw.write(reg_fields + "\n");
+		    	for(i = 0; i < numFields; i++){
+		    		if(i == numFields -1){
+		    			bw.write(reg_data[i] + "\n");
+		    		}
+		    		else{
+		    			bw.write(reg_data[i] + ",");
+		    		}
+		    	}
+		    	bw.write(ajira_reg);
+
+		    	bw = new BufferedWriter(new FileWriter(hadInfo.getAbsoluteFile(), true));
+		    	for(i = 0; i < numFields; i++){
+		    		if(i == numFields -1){
+		    			bw.write(output_data[i] + "\n");
+		    		}
+		    		else{
+		    			bw.write(output_data[i] + ",");
+		    		}
+		    	}
+	    	}
+	    	else{
+		    	ProcessBuilder pb = new ProcessBuilder(ajiraScriptFileName);
+		    	Process p = pb.start();
+		    	p.waitFor();
+		    	String time = System.getenv("EXEC_TIME");
+		    	
+		    	String[] output_data = {time, Long.toString(size)};
+		    	
+			    br = new BufferedReader(new FileReader(ajiInfo));
+			    br.readLine();
+			    String line;
+			    j = 0;
+			    while((line = br.readLine()) != null){
+			    	String[] tuple = line.split(",");
+			    	for(i = 0; i < numFields; i++){
+			    		if(i == 0){
+			    			y[j] = Double.parseDouble(tuple[i]);
+			    		}
+			    		else{
+			    			x[j][i-1] = Double.parseDouble(tuple[i]);
+			    		}
+			    	}
+			    	j++;
+			    }   
+			    for(i = 0; i < numFields; i ++){
+			    	if(i == 0){
+			    		y[j] = Double.parseDouble(output_data[i]);
+			    	}
+			    	x[j][i-1] = Double.parseDouble(output_data[i]);
+			    }
+			    
+			    mlr.newSampleData(y,x);
+			    double[] params = mlr.estimateRegressionParameters();
+			    
+		    	String[] reg_data = {Double.toString(params[0]), Double.toString(params[1])};	    	
+		    	
+		    	BufferedWriter bw = new BufferedWriter(new FileWriter(regInfo.getAbsoluteFile()));
+		    	bw.write(reg_fields + "\n");
+		    	bw.write(hadoop_reg + "\n");
+		    	for(i = 0; i < numFields; i++){
+		    		if(i == numFields -1){
+		    			bw.write(reg_data[i] + "\n");
+		    		}
+		    		else{
+		    			bw.write(reg_data[i] + ",");
+		    		}
+		    	}
+		    	
+		    	bw = new BufferedWriter(new FileWriter(ajiInfo.getAbsoluteFile(), true));
+		    	for(i = 0; i < numFields; i++){
+		    		if(i == numFields -1){
+		    			bw.write(output_data[i] + "\n");
+		    		}
+		    		else{
+		    			bw.write(output_data[i] + ",");
+		    		}
+		    	}
+	    	}
+	    	
 	    }
 	    
 	    //END
